05321847c8d79d1c11a22c83aaa4f7430e4d8f15
Bartosz Chojnacki <bartosz.chojnacki1@gmail.com>
2015-12-26 19:04:22 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,40 @@
+module.exports = function () {
+  "use strict";
+
+  var moduleName = 'angular-show-on-hover';
+
+  angular
+    .module(moduleName, [])
+    .directive('showOnHover', showOnHover);
+
+  function showOnHover($log) {
+
+    return {
+      restrict: 'A',
+      link: function (scope, element, attrs) {
+        if (!attrs.showOnHover) {
+          $log.error('You must specify child element in showOnHover attr');
+          return;
+        }
+        getChild(attrs.showOnHover).css('display', "none");
+
+
+        element.on('mouseenter', function () {
+          getChild(attrs.showOnHover).css('display', "");
+        });
+
+        element.on('mouseleave', function () {
+          getChild(attrs.showOnHover).css('display', "none");
+        });
+
+        function getChild(selector) {
+          return angular.element(element[0].querySelector(selector));
+        }
+      }
+    };
+  }
+
+  return moduleName;
+}();
+
+
